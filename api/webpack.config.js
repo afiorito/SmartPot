db79a1228f30abf8c09df03204a9644d19a45cea
Anthony <fioritoanthony@outlook.com>
2017-10-17 13:35:41 -0400

    Added serverless API boilerplate

---
+++ api/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.json',
+      '.ts',
+      '.tsx'
+    ]
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    loaders: [
+      { test: /\.ts(x?)$/, loader: 'ts-loader' },
+    ],
+  },
+};
