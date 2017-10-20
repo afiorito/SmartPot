67f0107e123844f879132dd9b2a53d255e813ffa
Anthony <fioritoanthony@outlook.com>
2017-10-20 08:03:37 -0400

    Completed CLD 2.2 - added an endpoint which can get and update the moisture of a plant in a smartpot

--- api/db/actions.js
+++ api/db/actions.js
@@ -6,6 +6,34 @@ import * as AWS from 'aws-sdk';
 import { documentClient } from './dynamoDB';
 
 export async function findSmartPot(potId) {
+  
+  const result = await getSmartPot(potId);
+  if (!result.Item) {
+    return { item: undefined, isFound: false };
+  }
+
+  return { item: result.Item, isFound: true };
+}
+
+export async function updateMoisture(moisture, potId) {
+  const params = {
+    TableName: 'smartpots',
+    Key: {
+      potId
+    },
+    UpdateExpression: 'set moisture = :moisture',
+    ExpressionAttributeValues: {
+      ':moisture': moisture
+    },
+    ConditionExpression: 'attribute_exists(potId)',
+    ReturnValues: 'ALL_NEW'
+  };
+
+  return execute('update', params);
+
+}
+
+export function getSmartPot(potId) {
   const getParams = {
     TableName: 'smartpots',
     Key: {
@@ -13,13 +41,7 @@ export async function findSmartPot(potId) {
     }
   };
 
-  const result = await execute('get', getParams);
-  if (!result.Item) {
-    return { item: undefined, isFound: false };
-  }
-
-  return { item: result.Item, isFound: true };
-
+  return execute('get', getParams);
 }
 
 function execute(action, params) {
---
+++ api/endpoints/moisture.js
@@ -0,0 +1,47 @@
+/**
+ * MOISTURE ENDPOINT
+ * GET: retrieve the moisture for a smartpot
+ * PUT: update the moisture of a smartpot
+ */
+
+import { findSmartPot, updateMoisture } from '../db/actions';
+import { failure, success } from './helpers/response-helper';
+
+export const main = async (event, context, cb) => {
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        cb(null, await get(event));
+        break;
+      case 'PUT':
+        cb(null, await put(event));
+        break;
+
+    };
+
+  } catch (e) {
+    cb(null, failure({ status: false }));
+  }
+};
+
+async function get(event) {
+  const potId = event.pathParameters.potId;
+
+  const smartpot = await findSmartPot(potId);
+
+  if(smartpot.isFound) {
+    return success({ moisture: smartpot.item.moisture });
+  }
+
+  return failure({ status: false });
+
+}
+
+async function put(event) {
+  const potId = event.pathParameters.potId;
+  const { moisture } = JSON.parse(event.body);
+
+  await updateMoisture(moisture, potId);
+
+  return success({ status: true });
+}
