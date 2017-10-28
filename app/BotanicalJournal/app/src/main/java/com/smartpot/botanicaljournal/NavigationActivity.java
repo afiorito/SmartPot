191500cda1418506aef488f106432ade94ef79c3
mg <marialgracedeleon@gmail.com>
2017-10-28 08:54:53 -0400

    Completed APP-3

---
+++ app/BotanicalJournal/app/src/main/java/com/smartpot/botanicaljournal/NavigationActivity.java
@@ -0,0 +1,72 @@
+package com.smartpot.botanicaljournal;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class NavigationActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set the sidebar navigation
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Display the main fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, PlantFragment.newInstance());
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (id == R.id.myPlants)
+            transaction.replace(R.id.frame_layout, PlantFragment.newInstance());
+        else if (id == R.id.addPlant) {
+            transaction.replace(R.id.frame_layout, AddPlantFragment.newInstance());
+        }
+        else if (id == R.id.settings) {
+            transaction.replace(R.id.frame_layout, SettingsFragment.newInstance());
+        }
+
+
+        transaction.commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/BotanicalJournal/app/src/main/java/com/smartpot/botanicaljournal/PlantFragment.java
@@ -0,0 +1,66 @@
+package com.smartpot.botanicaljournal;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class PlantFragment extends Fragment {
+    public static PlantFragment newInstance() {
+        PlantFragment fragment = new PlantFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_plant, container, false);
+
+        // Create temporary plants
+        ArrayList<Plant> plants = new ArrayList<>();
+        for (int i = 1; i <= 0 ; i++){
+            plants.add(new Plant("Plant " + i));
+        }
+
+        //Create plant adapter
+        PlantAdapter plantAdapter = new PlantAdapter(getContext(), plants);
+
+        //Get reference to ListView
+        ListView plantView = view.findViewById(R.id.plantListView);
+        LinearLayout noPlantsView =  view.findViewById(R.id.noPlantsView);
+
+        //Set ListView layout
+        plantView.setEmptyView(noPlantsView); // Set default layout when list is empty
+        plantView.setAdapter(plantAdapter);
+
+        // Replace main fragment with add plant fragment when button is clicked
+        Button addPlantButton = (Button) view.findViewById(R.id.addPlantButton);
+        addPlantButton.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        AddPlantFragment nextFrag= new AddPlantFragment();
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame_layout, nextFrag,"findThisFragment")
+                                .addToBackStack(null)
+                                .commit();
+
+                    }
+                }
+        );
+
+        return view;
+    }
+}
